531f3312ac987be9a84c482cd7795c1062c39d44
saeed <s.esmaiilzade@gmail.com>
2020-08-24 17:55:39 +0430

    added main.go, testInitilizeByStrValue

--- bitarray/bitarray.go
+++ bitarray/bitarray.go
@@ -1,8 +1,12 @@
 package bitarray
 
 import (
+	"fmt"
 	"math"
+	"strconv"
 )
+
+var blockLen int = 64
 
 //Bitarray array struct
 type Bitarray struct {
@@ -14,4 +18,44 @@ func (b *Bitarray) InitilizeBySize(size int) error {
 	storageSize := int64(math.Ceil(float64(size) / 64))
 	b.storage = make([]uint64, storageSize)
 	return nil
+}
+
+//InitilizeByStrValue take a value like "0110111001"
+func (b *Bitarray) InitilizeByStrValue(val string) error {
+
+	for val[0] == '0' {
+		val = val[1:]
+	}
+
+	size := len(val)
+	err := b.InitilizeBySize(size)
+	var tmp uint64
+
+	if err == nil {
+		i := len(b.storage) - 1
+		for len(val) >= blockLen && i >= 0 {
+			x := val[len(val)-blockLen:]
+			tmp, err = strconv.ParseUint(val[len(val)-blockLen:], 2, 64)
+			fmt.Println(x, tmp)
+			b.storage[i] = tmp
+			val = val[:len(val)-blockLen]
+			i--
+		}
+		if len(val) > 0 && i == 0 {
+			tmp, err = strconv.ParseUint(val, 2, 64)
+			b.storage[i] = tmp
+		} else {
+			err = fmt.Errorf("len(val)=%d, i=%d", len(val), i)
+		}
+	}
+
+	return err
+}
+
+//InitilizeByValue take a uint like 0b111010
+func (b *Bitarray) InitilizeByValue(val uint64) error {
+
+	err := b.InitilizeBySize(64)
+	b.storage[0] = val
+	return err
 }
--- bitarray/bitarray_test.go
+++ bitarray/bitarray_test.go
@@ -7,15 +7,37 @@ import (
 func TestBitarray(t *testing.T) {
 	checkInitBySize := func(t *testing.T, barr Bitarray, size int) {
 		t.Helper()
-		storage_size := len(barr.storage)
-		if size != storage_size {
-			t.Errorf("size = %d, but storage_size=%d", size, storage_size)
+		storagesize := len(barr.storage)
+		if size != storagesize {
+			t.Errorf("size = %d, but storagesize=%d\n", size, storagesize)
 		}
 
 	}
+
+	checkInitByStrValue := func(t *testing.T, barr Bitarray, storage []uint64) {
+		t.Helper()
+		size := len(barr.storage)
+		if len(storage) != size {
+			t.Errorf("size = %d, but storagesize=%d\n", size, len(storage))
+		}
+		for i := 0; i < size; i++ {
+			if barr.storage[i] != storage[i] {
+				t.Errorf("barr.storage[%d]=%d != storage=%d\n", i, barr.storage[i], storage[i])
+			}
+		}
+	}
+
 	t.Run("InitBySize", func(t *testing.T) {
 		barr := Bitarray{}
 		barr.InitilizeBySize(1000)
 		checkInitBySize(t, barr, 16)
+	})
+
+	t.Run("InitByeStrValue", func(t *testing.T) {
+		barr := Bitarray{}
+		barr.InitilizeByStrValue("11110101010101011110110100010100101010101000001011110010101011101010111110101011111011010001010010101010100000101111001010101110101011111010101111111111010101001010000001101001010100111010101010111010010101001111111111111111111111110101001010101001111111111111001010101001")
+		checkInitByStrValue(t, barr, []uint64{0b1111010101010101, 0b1110110100010100101010101000001011110010101011101010111110101011, 0b1110110100010100101010101000001011110010101011101010111110101011,
+			0b1111111101010100101000000110100101010011101010101011101001010100, 0b1111111111111111111111110101001010101001111111111111001010101001})
+
 	})
 }
